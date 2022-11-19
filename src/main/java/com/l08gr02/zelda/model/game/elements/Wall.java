3bbe5a7d24c343ce3f01b976a5049fbb58a63ee3
tomas-sucena <tomas21sucena@gmail.com>
2022-11-19 17:48:45 +0000

    first simple commit

--- src/main/java/com/l08gr02/zelda/model/game/elements/Wall.java
+++
@@ -1,8 +0,0 @@
-package com.l08gr02.zelda.model.game.elements;
-
-public class Wall extends Element{
-    // constructor
-    public Wall(int x, int y){
-        super(x, y);
-    }
-}
--- src/main/java/com/l08gr02/zelda/viewer/game/ElementViewer.java
+++
@@ -1,8 +0,0 @@
-package com.l08gr02.zelda.viewer.game;
-
-import com.l08gr02.zelda.gui.GUI;
-import com.l08gr02.zelda.model.game.elements.Element;
-
-public interface ElementViewer<T extends Element>{
-    void draw(T el, GUI gui);
-}
