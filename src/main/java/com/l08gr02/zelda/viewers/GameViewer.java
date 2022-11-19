cf66e44c75508251a757f15fd268643827743698
tomas-sucena <tomas21sucena@gmail.com>
2022-11-19 20:20:41 +0000

    created GameViewer

---
+++ src/main/java/com/l08gr02/zelda/viewers/GameViewer.java
@@ -0,0 +1,31 @@
+package com.l08gr02.zelda.viewers;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class GameViewer {
+    private final Screen screen;
+    private final TextGraphics graphics;
+
+    public GameViewer(int tWidth, int tHeight) throws IOException {
+        // criar o terminal
+        Terminal terminal = new DefaultTerminalFactory()
+                .setInitialTerminalSize(new TerminalSize(tWidth, tHeight))
+                .createTerminal();
+
+        // criar o screen
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null); // não precisamos do rato
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+
+        graphics = screen.newTextGraphics();
+    }
+}
---
+++ src/main/java/com/l08gr02/zelda/viewers/elements/ElementViewer.java
@@ -0,0 +1,7 @@
+package com.l08gr02.zelda.viewers.elements;
+
+import com.l08gr02.zelda.models.elements.Element;
+
+public interface ElementViewer<T extends Element> {
+    //public void draw(T el, GUI gui);
+}
