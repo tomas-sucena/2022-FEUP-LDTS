39a37debdcde39cb877af104529a124b56333cdd
tomas-sucena <tomas21sucena@gmail.com>
2022-11-25 00:57:17 +0000

    now git is mocking me :/

---
+++ src/test/java/com/l08gr02/zelda/PresenterTest.java
@@ -0,0 +1,32 @@
+package com.l08gr02.zelda;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.l08gr02.zelda.models.dungeon.Dungeon;
+import com.l08gr02.zelda.presenters.GameplayPresenter;
+import com.l08gr02.zelda.presenters.Presenter;
+import com.l08gr02.zelda.presenters.dungeon.DungeonPresenter;
+import com.l08gr02.zelda.viewers.dungeon.DungeonViewer;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class PresenterTest {
+    private Presenter presenter;
+    private TextGraphics graphics;
+
+    @Test
+    void DungeonPresenterTest(){
+        // ARRANGE
+        Dungeon model = Mockito.mock(Dungeon.class);
+        DungeonViewer viewer = Mockito.mock(DungeonViewer.class);
+
+        presenter = new DungeonPresenter(model, viewer);
+        graphics = Mockito.mock(TextGraphics.class);
+
+        // ACT
+        presenter.update(graphics, GameplayPresenter.ACTION.NOTHING);
+
+        // ASSERT
+        Mockito.verify(viewer, Mockito.times(1)).draw(graphics, model);
+    }
+
+}
