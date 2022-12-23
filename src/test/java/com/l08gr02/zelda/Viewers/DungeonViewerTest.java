150746140309e7c55ee41ef1104005ee50ca43ba
tomas-sucena <tomas21sucena@gmail.com>
2022-12-23 18:11:46 +0000

    fixed GrassViewerTest

--- src/test/java/com/l08gr02/zelda/Viewers/DungeonViewerTest.java
+++ src/test/java/com/l08gr02/zelda/Viewers/DungeonViewerTest.java
@@ -2,8 +2,10 @@ package com.l08gr02.zelda.Viewers;
 
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.l08gr02.zelda.gui.Camera;
 import com.l08gr02.zelda.gui.GUI;
 import com.l08gr02.zelda.models.dungeon.Dungeon;
+import com.l08gr02.zelda.models.elements.Position;
 import com.l08gr02.zelda.viewers.dungeon.DungeonViewer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,7 @@ import java.io.IOException;
 public class DungeonViewerTest {
     //Sussy baka depois preciso que testes e corrigas isto, desculpa my path sucks
     private GUI gui;
+    private Camera camera;
     private Dungeon dungeon;
     private DungeonViewer dungeonViewer;
     private TextGraphics tg;
@@ -22,25 +25,27 @@ public class DungeonViewerTest {
     void setup() throws IOException {
         dungeon = new Dungeon();
         dungeonViewer = new DungeonViewer();
+
         gui = Mockito.mock(GUI.class);
+        camera = Mockito.mock(Camera.class);
+        Mockito.when(gui.getCamera()).thenReturn(camera);
+        Mockito.when(camera.getPosition()).thenReturn(new Position(0, 0));
+
         tg = Mockito.mock(TextGraphics.class);
         Mockito.when(gui.getGraphics()).thenReturn(tg);
     }
     @Test
     void GrassdrawTest(){
-        dungeonViewer.draw(gui,dungeon);
+        dungeonViewer.draw(gui, dungeon);
         //Grass
-        Mockito.verify(tg,Mockito.times(1)).setBackgroundColor(TextColor.Factory.fromString("#6ADD4B")); //Deve estar a dar, nao sei se o numero esta certo though
-        Mockito.verify(tg,Mockito.times(1)).setBackgroundColor(TextColor.Factory.fromString("#3ABE41")); //Mesma coisa
-        Mockito.verify(tg,Mockito.times(1)).setBackgroundColor(TextColor.Factory.fromString("#29973B")); //Mesma coisa
-
-        //Brush
-        Mockito.verify(tg,Mockito.times(1)).setBackgroundColor(Mockito.any());
+        Mockito.verify(tg,Mockito.times(2460)).setBackgroundColor(TextColor.Factory.fromString("#6ADD4B"));
+        Mockito.verify(tg,Mockito.times(820)).setBackgroundColor(TextColor.Factory.fromString("#3ABE41"));
+        Mockito.verify(tg,Mockito.times(2460)).setBackgroundColor(TextColor.Factory.fromString("#29973B"));
     }
     @Test
     void BrushdrawTest(){
-        dungeonViewer.draw(gui,dungeon);
-        Mockito.verify(tg,Mockito.times(1)).setBackgroundColor(Mockito.any());
+        dungeonViewer.draw(gui, dungeon);
+        Mockito.verify(tg, Mockito.times(1)).setBackgroundColor(Mockito.any());
         Mockito.verify(tg, Mockito.times(1)).setCharacter(2,7,' ');
     }
 }
